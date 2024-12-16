795ef2b769e907fb0fe98d69feb4152f726918c7
Xuan Nghia <40400752+nghiax4@users.noreply.github.com>
2024-12-16 22:33:09 +0000

    dynamic job pages

--- database.py
+++ database.py
@@ -18,3 +18,14 @@ def load_jobs_from_db():
     for row in result.all():
       jobs.append(row._asdict())
     return jobs
+
+def load_job_from_db(id):
+  with engine.connect() as conn:
+    result = conn.execute(text(f"select * from jobs where id = {id}"))
+    rows = []
+    for row in result.all():
+      rows.append(row._asdict())
+    if len(rows) == 0:
+      return None
+    else:
+      return dict(rows[0])
